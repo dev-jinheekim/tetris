af25dd4e3a7e4a594baaea73df904bd15922a662
jay <mail@kimjinhee.me>
2018-08-24 17:20:54 +0900

    키보드입력 이벤트 추가, ES6 적용

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
-var block = document.getElementById('block');
-var trCount = 18;
-var tdCount = 10;
-var x = 0, y = Math.round(tdCount/2-1);
-var blockL = [
+const block = document.getElementById('block');
+const trCount = 18;
+const tdCount = 10;
+const x = 0, y = Math.round(tdCount/2-1);
+let blockL = [
         [x, y],
         [x+1, y],
         [x+2, y],
@@ -22,15 +22,15 @@ function createCol() {
 
 function createTable(target) {
 
-    var table = document.getElementById(target);
+    let table = document.getElementById(target);
 
-    for (var i = 0; i < trCount; i++) {
+    for (let i = 0; i < trCount; i++) {
 
-        var tr = createRow();
+        let tr = createRow();
 
-        for (var j = 0; j < tdCount; j++) {
+        for (let j = 0; j < tdCount; j++) {
 
-            var td = createCol();
+            let td = createCol();
             tr.appendChild(td);
 
         }
@@ -43,89 +43,78 @@ function createTable(target) {
 // 특정 칸의 색상을 변경
 function changeColor(tr, td, color) {
 
-    var x = block.childNodes.item(tr);
-    var y = x.childNodes.item(td);
+    let x = block.childNodes.item(tr);
+    let y = x.childNodes.item(td);
     y.style.backgroundColor = color;
 
 }
 
+
 // 블럭 생성
 function createBlock(block, color) {
 
-    for (var i = 0; i < blockL.length; i++) {
+    for (let i = 0; i < blockL.length; i++) {
         changeColor(block[i][0], block[i][1], color);
     }
 
 }
 
+// FIX : table  영역에서 벗어나지 않도록 수정
 // 블럭을 1칸 아래로 이동
 function moveBlockDown(block) {
 
-    for (var i = 0; i < block.length; i++) {
+    for (let i = 0; i < block.length; i++) {
         block[i][0] = block[i][0] + 1;
     }
 }
 
-
 // 블럭을 1칸 왼쪽으로 이동
 function moveBlockLeft(block) {
 
-    for (var i = 0; i < block.length; i++) {
+    for (let i = 0; i < block.length; i++) {
         block[i][1] = block[i][1] - 1;
     }
 }
 
-
 // 블럭을 1칸 오른쪽으로 이동
 function moveBlockRight(block) {
 
-    for (var i = 0; i < block.length; i++) {
+    for (let i = 0; i < block.length; i++) {
         block[i][1] = block[i][1] + 1;
     }
 }
 
 
+// 키입력 이벤트와 함수 연결
+document.addEventListener('keydown', (event) => {
+
+    const keyName = event.key;
+
+    if (keyName === 'ArrowDown') {
+        console.log('down');
+        moveBlockDown(blockL);
+        document.getElementById('block').innerHTML = '';
+        createTable('block');
+        createBlock(blockL, 'red');
+    }
+    if (keyName === 'ArrowLeft') {
+        console.log('down');
+        moveBlockLeft(blockL);
+        document.getElementById('block').innerHTML = '';
+        createTable('block');
+        createBlock(blockL, 'yellow');
+    }
+    if (keyName === 'ArrowRight') {
+        console.log('down');
+        moveBlockRight(blockL);
+        document.getElementById('block').innerHTML = '';
+        createTable('block');
+        createBlock(blockL, 'orange');
+    }
+
+});
+
 
 createTable('board');
 createTable('block');
-
-
 createBlock(blockL, 'white');
-
-
-setTimeout(
-    function(){
-        document.getElementById('block').innerHTML = '';
-        createTable('block');
-        moveBlockDown(blockL);
-        createBlock(blockL, 'red');
-    }, 1000
-);
-
-setTimeout(
-    function(){
-        document.getElementById('block').innerHTML = '';
-        createTable('block');
-        moveBlockDown(blockL);
-        createBlock(blockL, 'yellow');
-    }, 2000
-);
-
-setTimeout(
-    function(){
-        document.getElementById('block').innerHTML = '';
-        createTable('block');
-        moveBlockLeft(blockL);
-        createBlock(blockL, 'yellow');
-    }, 3000
-);
-
-setTimeout(
-    function(){
-        document.getElementById('block').innerHTML = '';
-        createTable('block');
-        moveBlockRight(blockL);
-        createBlock(blockL, 'yellow');
-    }, 4000
-);
-
