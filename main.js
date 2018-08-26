fda719a26d0fb861e8eb81da5b5f0374cf542423
jay <mail@kimjinhee.me>
2018-08-27 01:22:53 +0900

    class를 적용하여 구조변경 후 블록 표시

--- main.js
+++ main.js
@@ -2,40 +2,25 @@
 const board = document.getElementById('board');
 const trCount = 18; // y
 const tdCount = 10; // x
-const x = Math.round(tdCount/2-1), y = 0;
-let blockL = [
-    [x, y],
-    [x, y+1],
-    [x, y+2],
-    [x+1, y+2]
-];
+const x = Math.round(tdCount / 2 - 1), y = 0;
 
 
+class BlockL {
 
-// 테트리스 배경화면 생성
-function createRow() {
-    return document.createElement('TR');
-}
+    constructor(x, y, color) {
+        this.location = [x,y];
+        this.block = [
+            [[x, y], [x, y + 1], [x, y + 2], [x + 1, y + 2]],
+            [[x, y + 3], [x + 1, y], [x + 2, y], [x + 3, y]],
+            [[x, y], [x + 1, y], [x + 1, y + 1], [x + 1, y + 2]],
+            [[x, y], [x + 1, y + 1], [x + 1, y + 2], [x, y + 1]],
+        ];
+        this.turn = 0;
+        this.color = 'white';
+    }
 
-function createCol() {
-    return document.createElement('TD');
-}
-
-function createTable(target) {
-
-    let table = document.getElementById(target);
-
-    for (let i = 0; i < trCount; i++) {
-
-        let tr = createRow();
-
-        for (let j = 0; j < tdCount; j++) {
-
-            let td = createCol();
-            tr.appendChild(td);
-        }
-
-        table.appendChild(tr);
+    display() {
+        this.block[this.turn].forEach(location => changeColor(location[0], location[1], this.color));
     }
 
 }
@@ -51,94 +36,8 @@ function changeColor(tr, td, color) {
 }
 
 
-// 블럭 생성
-function createBlock(block, color) {
-
-    for (let i = 0; i < blockL.length; i++) {
-        changeColor(block[i][0], block[i][1], color);
-    }
-
-}
-
-
-// 블럭을 1칸 아래로 이동
-function moveBlockDown(block) {
-
-    let checkLocation = block.map(location => location[1]);
-    checkLocation.sort(function(a, b) { return a - b});
-
-    console.log(checkLocation);
-    if (checkLocation.pop() < trCount-1) {
-        for (let i = 0; i < block.length; i++) {
-            block[i][1] = block[i][1] + 1;
-        }
-    }
-
-}
-
-// 블럭을 1칸 왼쪽으로 이동
-function moveBlockLeft(block) {
-
-    let checkLocation = block.map(location => location[0]);
-    checkLocation.sort(function(a, b) { return a - b});
-    console.log(checkLocation);
-
-    if (checkLocation[0] >= 1) {
-        for (let i = 0; i < block.length; i++) {
-            block[i][0] = block[i][0] - 1;
-        }
-    }
-
-}
-
-// 블럭을 1칸 오른쪽으로 이동
-function moveBlockRight(block) {
-
-    let checkLocation = block.map(location => location[0]);
-    checkLocation.sort(function(a, b) { return a - b});
-    console.log(checkLocation);
-
-    if (checkLocation.pop() < tdCount-1) {
-        for (let i = 0; i < block.length; i++) {
-            block[i][0] = block[i][0] + 1;
-        }
-    }
-
-}
-
-
-// 키입력 이벤트와 함수 연결
-document.addEventListener('keydown', (event) => {
-
-    const keyName = event.key;
-
-    if (keyName === 'ArrowDown') {
-        console.log('down');
-        moveBlockDown(blockL);
-        document.getElementById('block').innerHTML = '';
-        createTable('block');
-        createBlock(blockL, 'red');
-    }
-
-    if (keyName === 'ArrowLeft') {
-        console.log('left');
-        moveBlockLeft(blockL);
-        document.getElementById('block').innerHTML = '';
-        createTable('block');
-        createBlock(blockL, 'yellow');
-    }
-
-    if (keyName === 'ArrowRight') {
-        console.log('right');
-        moveBlockRight(blockL);
-        document.getElementById('block').innerHTML = '';
-        createTable('block');
-        createBlock(blockL, 'orange');
-    }
-
-});
-
-
 createTable('background-table');
 createTable('board');
-createBlock(blockL, 'white');
+
+let blockL = new BlockL(x,y,'white');
+blockL.display();
